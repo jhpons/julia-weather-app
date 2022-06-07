ea3f2618e2840a93b8d42bb842076ba6b1c89366
jhpons <jhpons@gmail.com>
2022-06-06 21:40:34 -0400

    Remove Celsius conversion

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ function displayForecast(response) {
 }
 function getForecast(coordinates) {
   let apiKey = "6515f856935f19ac3c6d56e1e4dfc07c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 function displayCity(event) {
@@ -69,35 +69,15 @@ function displayCity(event) {
   let cityInput = document.querySelector("#city-search");
   h2 = document.querySelector("h2");
   h2.innerHTML = `${cityInput.value}`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=6515f856935f19ac3c6d56e1e4dfc07c&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=6515f856935f19ac3c6d56e1e4dfc07c&units=imperial`;
   axios.get(`${apiUrl}`).then(showTemperature);
 }
 
 let form = document.querySelector("#city");
 form.addEventListener("submit", displayCity);
 
-function showTempF(event) {
-  event.preventDefault();
-  let degrees = document.querySelector("#degrees");
-  celsius.classList.remove("active");
-  fahrenheit.classList.add("active");
-  degrees.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);
-}
-let fahrenheit = document.querySelector("#tempF");
-fahrenheit.addEventListener("click", showTempF);
-
-function showTempC(event) {
-  event.preventDefault();
-  let degrees = document.querySelector("#degrees");
-  celsius.classList.add("active");
-  fahrenheit.classList.remove("active");
-  degrees.innerHTML = Math.round(celsiusTemperature);
-}
-let celsius = document.querySelector("#tempC");
-celsius.addEventListener("click", showTempC);
-
 function showTemperature(response) {
-  celsiusTemperature = response.data.main.temp;
+  fahrenheitTemperature = response.data.main.temp;
   let temperature = Math.round(response.data.main.temp);
   let degrees = document.querySelector("#degrees");
   degrees.innerHTML = `${temperature}`;
@@ -117,7 +97,7 @@ function showTemperature(response) {
 function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=6515f856935f19ac3c6d56e1e4dfc07c&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=6515f856935f19ac3c6d56e1e4dfc07c&units=imperial`;
   axios.get(`${apiUrl}`).then(showTemperature);
 }
 function getCurrentPosition() {
@@ -129,4 +109,4 @@ button.addEventListener("click", getCurrentPosition);
 let dateTime = document.querySelector("#dateTime");
 dateTime.innerHTML = `${day} | ${hour}:${minutes}`;
 
-let celsiusTemperature = null;
+let fahrenheitTemperature = null;
